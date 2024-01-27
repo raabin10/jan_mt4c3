8f44cf6f6efdf9dde8af379205786ca1aed51967
raabin10 <raabinsinghasokkumar@gmail.com>
2024-01-27 21:43:36 +0530

    jan_mt4c4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NavBar from "./Components/NavBar";
+import Books from "./Components/Books";
+import MoreBooks from "./Components/MoreBooks";
+import BookDetails from "./Components/BookDetails";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState(undefined);
+  const [query, setQuery] = useState(null);
+
+  const [bookClicked, setBookClicked] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${
+          query ? query : "HarryPotter"
+        }&maxResults=30`
+      )
+      .then((res) => setBooks(res.data.items))
+      .catch((err) => console.log(err));
+    console.log(books);
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar setQuery={setQuery} setBookClicked={setBookClicked} />
+      {console.log(bookClicked)}
+      {bookClicked ? (
+        <BookDetails bookClicked={bookClicked} />
+      ) : books != undefined ? (
+        <Books booksDetail={books} setBookClicked={setBookClicked} />
+      ) : (
+        ""
+      )}
+
+      {books != undefined ? (
+        <MoreBooks booksDetail={books} setBookClicked={setBookClicked} />
+      ) : (
+        ""
+      )}
     </div>
   );
-}
+};
 
 export default App;
